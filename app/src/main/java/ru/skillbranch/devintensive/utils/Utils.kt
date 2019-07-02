86c8727746a0878330e5435094e6a51b880cbf07
dburinsky <dburinsky@gmail.com>
2019-07-02 17:20:27 +0300

    [hometask_2] sub-task 6 toInitials with test

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -11,13 +11,14 @@ object Utils {
         return firstName to lastName
     }
 
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return ("${firstName?.capitalize()?.take(1)?.getOrNull(0) ?: ""}" +
+                "${lastName?.capitalize()?.take(1)?.getOrNull(0) ?: ""}").trimIndent().ifEmpty { null }
+    }
+
     fun transliteration(fullName: String, divider: String = " "): String {
 //        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         return ""
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String? {
-        //     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        return ""
-    }
 }
