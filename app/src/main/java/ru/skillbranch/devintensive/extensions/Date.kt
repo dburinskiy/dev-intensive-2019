469430a6b81822703a436744359c8a71100a1832
dburinsky <dburinsky@gmail.com>
2019-07-03 00:10:36 +0300

    [hometask_2] sub-task 7 Date.humanizeDiff with test

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.absoluteValue
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String = SimpleDateFormat(pattern, Locale("ru")).format(this)
 
@@ -11,7 +13,54 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    return ""
+    val differenceInMilliseconds = this.time - date.time
+    var ago = differenceInMilliseconds < 0
+
+    return when (val seconds = TimeUnit.MILLISECONDS.toSeconds(differenceInMilliseconds.absoluteValue)) {
+        in 0..1 -> "только что"
+
+        in 1..45 -> agoOrFutureSuffix(ago, "несколько секунд")
+
+        in 45..75 -> agoOrFutureSuffix(ago, "минуту")
+
+        in 75..TimeUnit.MINUTES.toSeconds(45) -> agoOrFutureSuffix(
+            ago, pluralRuForms(
+                TimeUnit.SECONDS.toMinutes(seconds).toInt(),
+                arrayOf("минуту", "минуты", "минут")
+            )
+        )
+
+        in TimeUnit.MINUTES.toSeconds(45)..TimeUnit.MINUTES.toSeconds(75) -> agoOrFutureSuffix(ago, "час")
+
+        in TimeUnit.MINUTES.toSeconds(75)..TimeUnit.HOURS.toSeconds(22) -> agoOrFutureSuffix(
+            ago, pluralRuForms(
+                TimeUnit.SECONDS.toHours(seconds).toInt(),
+                arrayOf("час", "часа", "часов")
+            )
+        )
+
+        in TimeUnit.HOURS.toSeconds(22)..TimeUnit.HOURS.toSeconds(26) -> agoOrFutureSuffix(ago, "день")
+
+        in TimeUnit.HOURS.toSeconds(26)..TimeUnit.DAYS.toSeconds(360) -> agoOrFutureSuffix(
+            ago, pluralRuForms(
+                TimeUnit.SECONDS.toDays(seconds).toInt(),
+                arrayOf("день", "дня", "дней")
+            )
+        )
+        else -> if (ago) "более года назад" else "более чем через год"
+    }
+
+}
+
+private fun agoOrFutureSuffix(ago: Boolean, text: String): String = if (ago) "${text} назад" else "через ${text}"
+
+//http://docs.translatehouse.org/projects/localization-guide/en/latest/l10n/pluralforms.html?id=l10n/pluralforms
+private fun pluralRuForms(n: Int, titles: Array<String>): String {
+    val i =
+        if (n % 10 == 1 && n % 100 != 11) 0
+        else if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) 1
+        else 2
+    return "${n} ${titles[i]}"
 }
 
 enum class TimeUnits(val milliseconds: Long) {
